85047327294e6399f574a1ca34e58cb898627d2b
Kartikv313 <kartikv313@gmail.com>
2025-06-01 09:53:40 +0530

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav m-auto my-2 text-center">
-                        <li className="nav-item">
+                    <li className="nav-item">
                             <NavLink className="nav-link" to="/">Home </NavLink>
                         </li>
                         <li className="nav-item">
